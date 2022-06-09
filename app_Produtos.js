ff7d8cca5ae667be3ce4bcb23cda58a6aa45b09f
Ricardo Teixeira <rjtsjr@gmail.com>
2022-06-09 20:34:38 -0300

    Inclusão de itens na cesta
    o JavaScript cria a lista dos produtos, caso não exista no LocalStorage e a página de Produtos adiciona sempre uma quantidade na Cesta, referente ao produto comprado.

---
+++ app_Produtos.js
@@ -0,0 +1,181 @@
+/* Carregar Produtos */
+		var produtosList;
+		var produtosOutput;
+		
+		window.addEventListener("load",function(){
+			//guarda em uma variável os elementos da main-produtos
+			produtosOutput = document.getElementById("galleryproducts");
+			if(localStorage.getItem("produtos")){
+				produtosList = JSON.parse(localStorage.getItem("produtos"));
+			}else{
+				produtosList = [];
+			}
+			
+			if(produtosList.length == 0){
+				IncluirProdutos();
+			}
+			//adiciona o listener para o evento submit, utilizei form para usar o required do input HTML
+			//document.getElementById("galleryproducts").addEventListener("submit",onSubmit);
+			//produtosOutput.addEventListener("click",clickButton);
+		})
+		
+		function IncluirProdutos() {
+			//incluir manualmente os produtos que serão vendidos
+			var item = {};
+			item.id = 0;
+			item.img = "imagens/Produto1.png";
+			item.nome = "Biscoito Amanteigado Goiaba 100g";
+			item.quantidade = 0;
+			item.unit = 15;
+			item.valor = 0;
+			item.excluir = "false";
+			//adicionando a variável na lista
+			produtosList.push(item);
+			saveList();
+
+			var item = {};
+			item.id = 1;
+			item.img = "imagens/Produto2.png";
+			item.nome = "Biscoito Amanteigado Ervas Finas 50g";
+			item.quantidade = 0;
+			item.unit = 7;
+			item.valor = 0;
+			item.excluir = "false";
+			//adicionando a variável na lista
+			produtosList.push(item);
+			saveList();
+
+			var item = {};
+			item.id = 2;
+			item.img = "imagens/Produto3.png";
+			item.nome = "Biscoito Amanteigado Chocolate 50g";
+			item.quantidade = 0;
+			item.unit = 8;
+			item.valor = 0;
+			item.excluir = "false";
+			//adicionando a variável na lista
+			produtosList.push(item);
+			saveList();
+
+			var item = {};
+			item.id = 3;
+			item.img = "imagens/Produto4.png";
+			item.nome = "Biscoito Amanteigado Cebola 100g";
+			item.quantidade = 0;
+			item.unit = 16;
+			item.valor = 0;
+			item.excluir = "false";
+			//adicionando a variável na lista
+			produtosList.push(item);
+			saveList();
+
+			var item = {};
+			item.id = 4;
+			item.img = "imagens/Produto5.png";
+			item.nome = "Biscoito Amanteigado Queijo 100g";
+			item.quantidade = 0;
+			item.unit = 16;
+			item.valor = 0;
+			item.excluir = "false";
+			//adicionando a variável na lista
+			produtosList.push(item);
+			saveList();
+
+			var item = {};
+			item.id = 5;
+			item.img = "imagens/Produto6.png";
+			item.nome = "Bolo de Aniversário 10cm e 02 Doces";
+			item.quantidade = 0;
+			item.unit = 20;
+			item.valor = 0;
+			item.excluir = "false";
+			//adicionando a variável na lista
+			produtosList.push(item);
+			saveList();
+
+			var item = {};
+			item.id = 6;
+			item.img = "imagens/Produto7.png";
+			item.nome = "Caixa de Bolo no Pote";
+			item.quantidade = 0;
+			item.unit = 30;
+			item.valor = 0;
+			item.excluir = "false";
+			//adicionando a variável na lista
+			produtosList.push(item);
+			saveList();
+
+			var item = {};
+			item.id = 7;
+			item.img = "imagens/Produto8.png";
+			item.nome = "Caneca com Cookie";
+			item.quantidade = 0;
+			item.unit = 55;
+			item.valor = 0;
+			item.excluir = "false";
+			//adicionando a variável na lista
+			produtosList.push(item);
+			saveList();
+
+			var item = {};
+			item.id = 8;
+			item.img = "imagens/Produto9.png";
+			item.nome = "Doces Diversos";
+			item.quantidade = 0;
+			item.unit = 4;
+			item.valor = 0;
+			item.excluir = "false";
+			//adicionando a variável na lista
+			produtosList.push(item);
+			saveList();
+		}
+
+		function clickButton1(){
+			produtosList[0].quantidade++;
+			saveList();
+		}
+
+		function clickButton2(){
+			produtosList[1].quantidade++;
+			saveList();
+		}
+
+		function clickButton3(){
+			produtosList[2].quantidade++;
+			saveList();
+		}
+
+		function clickButton4(){
+			produtosList[3].quantidade++;
+			saveList();
+		}
+
+		function clickButton5(){
+			produtosList[4].quantidade++;
+			saveList();
+		}
+
+		function clickButton6(){
+			produtosList[5].quantidade++;
+			saveList();
+		}
+
+		function clickButton7(){
+			produtosList[6].quantidade++;
+			saveList();
+		}
+
+		function clickButton8(){
+			produtosList[7].quantidade++;
+			saveList();
+		}
+
+		function clickButton9(){
+			produtosList[8].quantidade++;
+			saveList();
+		}
+
+		function saveList(){
+			//converte os dados em string e salva no local storage 
+			localStorage.setItem("produtos",JSON.stringify(produtosList));
+		}
---
+++ app_lixo.js
@@ -0,0 +1,95 @@
+/* Carregar Produtos */
+		var itensList;
+		var itensOutput;
+		
+		window.addEventListener("load",function(){
+			//guarda em uma variável o elemento itens-output
+			itensOutput = document.getElementById("itens-output")
+			if(localStorage.getItem("produtos")){
+				itensList = JSON.parse(localStorage.getItem("produtos"));
+				showList()
+			}else{
+				itensList = [];
+			}
+			
+			if(itensList.length == 0){
+				itensOutput.innerHTML = "Nenhum item cadastrado"
+			}
+			//adiciona o listener para o evento submit, utilizei form para usar o required do input HTML
+			document.getElementById("form-itens").addEventListener("submit",onSubmit);
+			itensOutput.addEventListener("click",clickList)
+		})
+		
+		function clickList(e){
+			//somente fazer algo quando clicar em um item li
+			if(e.target.localName == "li"){
+				e.target.dataset.excluir = (e.target.dataset.excluir === 'true')? false : true;
+				itensList[e.target.dataset.id].excluir = e.target.dataset.excluir;
+				saveList();
+			}else if(e.target.localName == "button"){
+				clearList()
+			}
+		}
+
+		function onSubmit(e){
+			var task = {};
+			//pego o valor cadastrado nos inputs do form
+			task.id = itensList.length;
+			task.img = "";
+			task.nome = e.target[0].value;
+			task.quantidade = e.target[1].value;
+			task.unit = e.target[2].value;
+			task.valor = 0;
+			task.excluir = "false";
+			
+			//adicionando a task na lista
+			itensList.push(task);
+			saveList();
+			showList();
+			// utiliza o preventDefault para evitar do form realizar o reload da página
+			e.preventDefault();
+		}
+		
+		function saveList(){
+			//converte os dados em string e salva no local storage 
+			localStorage.setItem("produtos",JSON.stringify(itensList));
+		}
+
+		function showList(){
+			//mostra a lista de itens
+			var total = itensList.length;
+			var totvalor = 0;
+			if(total > 0){
+				var htmlTemp = "<ul>"; 
+				for(var i = 0; i < total; i++){
+					if(itensList[i].quantidade > 0){
+						itensList[i].valor = itensList[i].quantidade*itensList[i].unit;
+						totvalor = totvalor+itensList[i].valor;
+						htmlTemp += "<li data-id='"+itensList[i].id+"' data-excluir='" + itensList[i].excluir + "'>"+ itensList[i].nome + " - Quantidade: "+ itensList[i].quantidade + " - Valor: R$ "+ itensList[i].valor + "</li>"
+					}
+				}
+				if(totvalor > 0){
+					htmlTemp += "<h2> Total da compra: R$ "+totvalor+"</h2>";
+				    itensOutput.innerHTML = htmlTemp;
+				}
+				htmlTemp += "</ul><button>Limpar itens Marcados</button>";
+				itensOutput.innerHTML = htmlTemp;
+			}else{
+				itensOutput.innerHTML = "<h2> Nenhum item cadastrado </h2>"
+			}
+		}
+
+		function clearList(){
+			//varre a lista a procura de tarefas realizadas
+			for(var i = 0; i < itensList.length; i++){
+				if(itensList[i].excluir === 'true'){
+					itensList.splice(i, 1);  //remove 1 elemento na posição i;
+					i = 0;  //voltando o indice no array para validar novamente a lista
+				}else{
+					itensList[i].id = i;
+				}
+			}
+			showList();
+			saveList();
+		}
+		
